99183b853689076942047b716fd5092fe33f9102
rmgilhan <rmgilhan@gmail.com>
2025-04-26 17:06:03 +0800

    just slight modification of index.js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const dotenv = require('dotenv').config();
-const express = require("express");
-const mongoose = require("mongoose");
-
+const express = require('express');
+const mongoose = require('mongoose');
 const session = require('express-session');
-
-// Allows our backend application to be available to our frontend application
-// Allows us to control the app's Cross Origin Resource Sharing settings
 const cors = require('cors');
 const userRoutes = require('./routes/user');
 const menuRoutes = require('./routes/menu');
 const restaurantRoutes = require('./routes/restaurant');
-
 const cartRoutes = require('./routes/cart');
 const orderRoutes = require('./routes/order');
 const reservationRoutes = require('./routes/reservation');
@@ -18,34 +13,37 @@ const reservationRoutes = require('./routes/reservation');
 const app = express();
 const port = 4005;
 
+// Ensure you are loading the environment variables from .env
+if (dotenv.error) {
+    throw new Error("Couldn't load .env file");
+}
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+// Set up the session middleware with environment variables
 app.use(session({
-	secret: ("/auth"),
-	resave: false,
-	saveUninitialized: false
+    secret: process.env.SESSION_SECRET || "/auth", // Use SESSION_SECRET from .env or fallback
+    resave: false,
+    saveUninitialized: false
 }));
 
-//mongoose.connect("mongodb+srv://rmgilhan:t9eng@cluster0.tx2qodg.mongodb.net/myRestoBar?retryWrites=true&w=majority&appName=Cluster0");
-mongoose.connect("mongodb+srv://nmciran97:g2oRC6V7rDB1U2J4@cluster0.mvp0qmo.mongodb.net/myRestoBar?retryWrites=true&w=majority&appName=Cluster0");
-//mongoose.connect("mongodb://localhost:27017/myRestoBar?retryWrites=true&w=majority&appName=Cluster0");
+// Connect to MongoDB using the MONGODB_URI environment variable
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch((err) => console.log('MongoDB connection error:', err));
 
-mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas.'));
 
-app.use("/resto/users",  userRoutes);
+app.use("/resto/users", userRoutes);
 app.use("/resto/menus", menuRoutes);
 app.use("/resto/restoBuild", restaurantRoutes);
 app.use("/resto/cart", cartRoutes);
-app.use("/resto/order", orderRoutes)
-app.use("/resto/reservation", reservationRoutes)
+app.use("/resto/order", orderRoutes);
+app.use("/resto/reservation", reservationRoutes);
 
-
-//"process.env.PORT || port" will use the environment variable if it is available OR will used port 4000 if none defined
-//This syntax will allow flexibility when using the application locally
-if(require.main === module){
-	app.listen(port, () => console.log(`API is now online on port ${port}`));
-
+if (require.main === module) {
+    app.listen(port, () => console.log(`API is now online on port ${port}`));
 }
-module.exports = {app, mongoose};
+
+module.exports = { app, mongoose };
